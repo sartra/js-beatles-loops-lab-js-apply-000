001cf46e893ef8b02bff27230b91879ee6b06f9c
Sarah Renshaw <strenshaw@gmail.com>
2017-06-05 04:22:01 +0000

    Done.

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ function johnLennonFacts(facts) {
   var i=0;
   while (i < facts.length) {
     shoutedFacts.push (`${facts[i]}!!!`);
+    i++;
   }
   return shoutedFacts;
 }
 
-//
-// function iLoveTheBeatles(num) {
-//   const arr = [];
-//   do {
-//     arr.push('I love the Beatles!')
-//     num++;
-//   }
-//   while (num<15);
-//
-//   return arr;
-// }
+
+function iLoveTheBeatles(num) {
+  const arr = [];
+  do {
+    arr.push('I love the Beatles!')
+    num++;
+  }
+  while (num<15);
+
+  return arr;
+}
